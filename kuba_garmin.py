f755b38a9c0cf104f5459596129d573340be0846
Tomaicho <tomas.a.lima@gmail.com>
2025-03-17 23:51:23 +0100

    Update kuba_garmin.py to only import main_menu_loop from utils

--- kuba_garmin.py
+++ kuba_garmin.py
@@ -1,23 +1,3 @@
-import datetime
-from datetime import timezone
-import json
-import logging
-import os
-import sys
-import credentials
-from getpass import getpass
-
-import readchar
-import requests
-from garth.exc import GarthHTTPError
-
-from garminconnect import (
-    Garmin,
-    GarminConnectAuthenticationError,
-    GarminConnectConnectionError,
-    GarminConnectTooManyRequestsError,
-)
-
-from kuba_utils import *
+from kuba_utils import main_menu_loop
 
 main_menu_loop()
